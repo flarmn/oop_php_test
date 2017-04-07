274da7c7dc56c01a0a3d380fef5119e508f56565
Unknown <flarmn@gmail.com>
2017-04-07 16:19:31 +0300

    Initial php markup according to task

---
+++ index.php
@@ -0,0 +1,43 @@
+ <?php
+
+
+
+class Node
+{
+    // properties
+    public $child = 'значение по умолчанию';
+
+    // methods
+    public function addNode($i) {
+
+    }
+
+
+    public function deleteNode($i) {
+
+    }
+
+
+    public function output() {
+
+    }
+
+
+     public function getType() {
+
+    }
+
+}//Node
+
+
+class RootNode{
+
+}//RootNode
+
+
+class BranchNode{
+	
+}//BranchNode
+
+
+?> 
