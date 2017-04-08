8e796d256e7599807fb329e9e6ee69940768696b
Unknown <flarmn@gmail.com>
2017-04-08 12:23:14 +0300

    deletting objects, reset view

--- creating_objects.php
+++ creating_objects.php
@@ -39,14 +39,45 @@ print_r(get_defined_vars());
 echo '</pre>';
 */
 
-//print_r($GLOBALS['obj']);
+print_r($GLOBALS['obj']);
 
-$rr = (array_keys(get_object_vars($obj)));
-for ($i = 1; $i< count($rr); $i++){
-echo ($rr[$i]). '<br/>';
-}
+getObjectsList($obj);
 //echo ($rr);
 
 //print_r(get_defined_vars('obj'));
 //var_dump($node);
+
+unset($obj->objs2);
+//$objs2 = NULL;
+
+
+ob_clean();
+
+echo 'chuchu' . '<br/>';
+
+
+//print_r($GLOBALS['obj']);
+
+/*
+$rr = (array_keys(get_object_vars($obj)));
+for ($i = 1; $i< count($rr); $i++){
+echo ($rr[$i]). '<br/>';
+}
+*/
+
+getObjectsList($obj);
+
+
+function getObjectsList($obj){
+	echo '<br/>';
+	echo 'OBject list';
+	echo '<br/>';	
+$rr = (array_keys(get_object_vars($obj)));
+for ($i = 1; $i< count($rr); $i++){
+echo ($rr[$i]). '<br/>';
+}
+}//func
+
+
+
 ?> 
