521b94e08ce63175d3977e80253883428ca807d9
Unknown <flarmn@gmail.com>
2017-04-09 12:00:13 +0300

    work great oop, no oop and index

--- add_delete_links_working_with_get_oop.php
+++ add_delete_links_working_with_get_oop.php
@@ -1,9 +1,24 @@
  <?php
 
+ class Node{
+public function output(){
+for ($i = 1; $i < $_GET["node"]+1; $i++){
+echo '<a href="?command=delete&node='  . $objcount=$_GET["node"]-1 .   '"> delete object' . $i . ' </a><br />';
+}
+ 	}
+ }
+
+
+$Node = new Node();
+//$objcount=$_GET["node"]+1;
 echo '<a href="?command=addobject&node=' . $objcount=$_GET["node"]+1 . '">addobject</a><br />';
 
-for ($i = 1; $i < $_GET["node"]; $i++){
-echo '<a href="?command=delete&node='  . $objcount=$_GET["node"]-1 .   '"> delete object' . $i . ' </a><br />';
+if($_GET["command"]=="addobject"){
+$Node->output();
+}
+
+if($_GET["command"]=="delete"){
+$Node->output();
 }
 
 //END
